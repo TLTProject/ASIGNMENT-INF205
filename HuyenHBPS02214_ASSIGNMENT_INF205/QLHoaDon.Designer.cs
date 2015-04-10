dbecf73b23c99383b74fd5f5ffe1f14579f57a3d
hbhcenter <khaitrac.sir@gmail.com>
2015-04-11 01:26:27 +0700

    Tạo form QLHoaDon
    Giao diện form QLHoaDon

---
+++ HuyenHBPS02214_ASSIGNMENT_INF205/QLHoaDon.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace HuyenHBPS02214_ASSIGNMENT_INF205
+{
+    partial class frmQLHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtThongSo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvKetQua = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtThongSo
+            // 
+            this.txtThongSo.Location = new System.Drawing.Point(341, 32);
+            this.txtThongSo.Name = "txtThongSo";
+            this.txtThongSo.Size = new System.Drawing.Size(246, 20);
+            this.txtThongSo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thông số:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(593, 30);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // dgvKetQua
+            // 
+            this.dgvKetQua.AllowUserToAddRows = false;
+            this.dgvKetQua.BackgroundColor = System.Drawing.Color.White;
+            this.dgvKetQua.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKetQua.GridColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvKetQua.Location = new System.Drawing.Point(82, 100);
+            this.dgvKetQua.Name = "dgvKetQua";
+            this.dgvKetQua.RowHeadersVisible = false;
+            this.dgvKetQua.Size = new System.Drawing.Size(842, 383);
+            this.dgvKetQua.TabIndex = 3;
+            // 
+            // frmQLHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 526);
+            this.Controls.Add(this.dgvKetQua);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtThongSo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmQLHoaDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Quản lý hoá đơn";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKetQua)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtThongSo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvKetQua;
+    }
+}
